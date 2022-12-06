8c113f57d179b5bde3ac7254b187fb8df09b6a68
AtmecsAnanth333 <116054577+AtmecsAnanth333@users.noreply.github.com>
2022-12-06 21:47:17 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+function test() {
+    console.log("function called");
+}
+test();
+//function defined 
+function greet() {
+    return "Hello World";
+}
+function caller() {
+    var msg = greet(); //function greet() invoked 
+    console.log(msg);
+}
+//invoke function 
+caller();
+function test_param(n1, s1) {
+    console.log(n1);
+    console.log(s1);
+}
+test_param(123, "this is a string");
